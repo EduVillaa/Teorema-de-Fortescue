05586d5374d54fbb94273da6068cb6dd64733d88
EduVillaa <eduardovllvrd@gmail.com>
2025-09-24 13:33:10 +0200

    Update Teorema de Fortescue.py

--- Teorema de Fortescue.py
+++ Teorema de Fortescue.py
@@ -2,22 +2,37 @@
 #uno de secuencia directa, uno de secuencia inversa, y uno de secuencia homopolar
 import matplotlib.pyplot as plt
 import numpy as np
+import cmath as cmath
 
 Alpha_real=np.cos(120*np.pi/180)
 Alpha_im=np.sin(120*np.pi/180)
 Alpha=complex(Alpha_real, Alpha_im)
-print(Alpha)
 
-Vfase=[5+2j, -4+8j, -5j]
+Vfase=[8+6j, 8j, -2-8j]
 T=[[1, 1, 1,],
    [1, Alpha**2, Alpha],
    [1, Alpha, Alpha**2]]
 
 Vsecuencia_fase_a=np.linalg.inv(T)@Vfase
 
-Vsecuencias=[[Vsecuencia_fase_a[0], Vsecuencia_fase_a[1], Vsecuencia_fase_a[2] ],
-             [Vsecuencia_fase_a[0], Vsecuencia_fase_a[1]*Alpha**2, Vsecuencia_fase_a[2]*Alpha],
-             [Vsecuencia_fase_a[0],Vsecuencia_fase_a[1]*Alpha, Vsecuencia_fase_a[2]*Alpha**2] ]
+
+
+Vsecuencias=[[Vsecuencia_fase_a[0], Vsecuencia_fase_a[1], Vsecuencia_fase_a[2]], #Secuencia homopolar (azul)
+             [Vsecuencia_fase_a[0], Vsecuencia_fase_a[1]*Alpha**2, Vsecuencia_fase_a[2]*Alpha], #Secuencia directa (verde)
+             [Vsecuencia_fase_a[0],Vsecuencia_fase_a[1]*Alpha, Vsecuencia_fase_a[2]*Alpha**2]] #Secuencia inversa (rojo)
+
+Vsec=["Secuencia homopolar:", "Secuencia directa:", "Secuencia inversa:"]
+fase=["A", "B", "C"]
+
+for i in range(3):
+    print("")
+    print(Vsec[i])
+    print("")
+    for c in range(3):
+        mag=abs(Vsecuencias[c][i])
+        arg_grados = cmath.phase(Vsecuencias[c][i])*180/np.pi
+        print("Fase", fase[c], ":", round(mag, 2),"∠",round(arg_grados, 2))
+    
 
 plt.xlim(-10, 10)
 plt.ylim(-10, 10)
@@ -25,8 +40,7 @@ plt.axhline(0, color="k", lw=0.2)
 plt.axvline(0, color="k", lw=0.2)
 plt.grid()
 
-colores=["blue", "green", "red", "orange", "purple", "navy", "pink",
-         "gray", "olive", "teal", "navy"]
+colores=["blue", "green", "red", "orange", "purple", "navy"]
 
 for i in Vsecuencias:
     xorigen=0
@@ -48,7 +62,6 @@ for i in Vfase:
     c+=1
 
 plt.show()
-
 
 
 
