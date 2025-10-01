488e7876258fb4cca7108f31e5fb4a08bf6e2b6f
EduVillaa <eduardovllvrd@gmail.com>
2025-10-02 00:03:07 +0200

    Update Programa con streamlit.py

--- Programa con streamlit.py
+++ Programa con streamlit.py
@@ -22,11 +22,11 @@ col1, col2, col3 =st.columns([2,1,1])
 
 with col2:
     modulofa = st.slider("Módulo fase A", 0.0, 10.0, 5.0, 0.1)
-    angulofa = st.slider("Arg fase A", 0.0, 360.0, 30.0, 1.0)
+    angulofa = st.slider("Arg fase A", 0.0, 360.0, 90.0, 1.0)
     modulofb = st.slider("Módulo fase B", 0.0, 10.0, 5.0, 0.1)
-    angulofb = st.slider("Arg fase B", 0.0, 360.0, 150.0, 1.0)
+    angulofb = st.slider("Arg fase B", 0.0, 360.0, -30.0, 1.0)
     modulofc = st.slider("Módulo fase C", 0.0, 10.0, 5.0, 0.1)
-    angulofc = st.slider("Arg fase C", 0.0, 360.0, 90.0, 1.0)
+    angulofc = st.slider("Arg fase C", 0.0, 360.0, -150.0, 1.0)
     
 with col1:
     fig, ax = plt.subplots()
@@ -41,11 +41,60 @@ with col1:
     faX, faY = polar2cart(modulofa, angulofa)
     fbX, fbY = polar2cart(modulofb, angulofb)
     fcX, fcY = polar2cart(modulofc, angulofc)
-    faseA = ax.quiver(0, 0, faX, faY, angles="xy", scale_units="xy", scale=1, color="blue", label="Fase A")
-    faseB = ax.quiver(0, 0, fbX, fbY, angles="xy", scale_units="xy", scale=1, color="green", label="Fase B")
-    faseC = ax.quiver(0, 0, fcX, fcY, angles="xy", scale_units="xy", scale=1, color="red", label="Fase C")
+    faseA = ax.quiver(0, 0, faX, faY, angles="xy", scale_units="xy", scale=1, color="cornflowerblue", label="Fase A")
+    faseB = ax.quiver(0, 0, fbX, fbY, angles="xy", scale_units="xy", scale=1, color="tomato", label="Fase B")
+    faseC = ax.quiver(0, 0, fcX, fcY, angles="xy", scale_units="xy", scale=1, color="mediumseagreen", label="Fase C")
     ax.legend()
+    #Fase homopolar
+    zfaseA = complex(faX, faY)
+    zfaseB = complex(fbX, fbY)
+    zfaseC = complex(fcX, fcY)
+    dxH = ((zfaseA+zfaseB+zfaseC)*1/3).real
+    dyH = ((zfaseA+zfaseB+zfaseC)*1/3).imag
+    secH = ax.quiver(0, 0, dxH, dyH, angles="xy", scale_units="xy", scale=1, color="k")
+    #Fase directa
+    alphaX, alphaY = polar2cart(1, 120)
+    alpha = complex(alphaX, alphaY)
+    sec_directa_faseAz = (zfaseA+zfaseB*alpha+zfaseC*alpha**2)*1/3
+    sec_directa_faseBz = sec_directa_faseAz*alpha**2
+    sec_directa_faseCz = sec_directa_faseAz*alpha
+    secD_faseAz = ax.quiver(dxH, dyH, sec_directa_faseAz.real, sec_directa_faseAz.imag, angles="xy", scale_units="xy", scale=1, color="lightcoral")
+    secD_faseBz = ax.quiver(dxH, dyH, sec_directa_faseBz.real, sec_directa_faseBz.imag, angles="xy", scale_units="xy", scale=1, color="lightcoral")
+    secD_faseCz = ax.quiver(dxH, dyH, sec_directa_faseCz.real, sec_directa_faseCz.imag, angles="xy", scale_units="xy", scale=1, color="lightcoral")
+    #Fase inversa
+    sec_inversa_faseAz = (zfaseA+zfaseB*alpha**2+zfaseC*alpha)*1/3
+    sec_inversa_faseBz = sec_inversa_faseAz*alpha
+    sec_inversa_faseCz = sec_inversa_faseAz*alpha**2
+    secI_faseAz = ax.quiver(sec_directa_faseAz.real+dxH, sec_directa_faseAz.imag+dyH, sec_inversa_faseAz.real, sec_inversa_faseAz.imag, angles="xy", scale_units="xy", scale=1, color="khaki")
+    secI_faseBz = ax.quiver(sec_directa_faseBz.real+dxH, sec_directa_faseBz.imag+dyH, sec_inversa_faseBz.real, sec_inversa_faseBz.imag, angles="xy", scale_units="xy", scale=1, color="khaki")
+    secI_faseCz = ax.quiver(sec_directa_faseCz.real+dxH, sec_directa_faseCz.imag+dyH, sec_inversa_faseCz.real, sec_inversa_faseCz.imag, angles="xy", scale_units="xy", scale=1, color="khaki")
+    
     fig.tight_layout()
     st.pyplot(fig, clear_figure=True)
     
-
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
