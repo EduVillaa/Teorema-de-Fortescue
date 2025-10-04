e3be4273c63f2ef44ba5c14432f07667ab8260b6
EduVillaa <eduardovllvrd@gmail.com>
2025-10-04 23:19:38 +0200

    Update Programa con streamlit.py

--- Programa con streamlit.py
+++ Programa con streamlit.py
@@ -1,8 +1,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import streamlit as st
+from math import atan2, degrees
 
 polar2cart = lambda mod, ang: (mod*np.cos(np.pi/180*ang), mod*np.sin(np.pi/180*ang))
+cart2polar = lambda dx, dy: (np.sqrt(dx**2+dy**2), degrees(atan2(dy, dx)))
 
 st.set_page_config(layout="wide")
 st.title("Teorema de Fortescue")
@@ -24,9 +26,9 @@ with col2:
     modulofa = st.slider("Módulo fase A", 0.0, 10.0, 5.0, 0.1)
     angulofa = st.slider("Arg fase A", 0.0, 360.0, 90.0, 1.0)
     modulofb = st.slider("Módulo fase B", 0.0, 10.0, 5.0, 0.1)
-    angulofb = st.slider("Arg fase B", 0.0, 360.0, -30.0, 1.0)
+    angulofb = st.slider("Arg fase B", 0.0, 360.0, 330.0, 1.0)
     modulofc = st.slider("Módulo fase C", 0.0, 10.0, 5.0, 0.1)
-    angulofc = st.slider("Arg fase C", 0.0, 360.0, -150.0, 1.0)
+    angulofc = st.slider("Arg fase C", 0.0, 360.0, 210.0, 1.0)
     
 with col1:
     fig, ax = plt.subplots()
@@ -67,10 +69,30 @@ with col1:
     sec_inversa_faseCz = sec_inversa_faseAz*alpha**2
     secI_faseAz = ax.quiver(sec_directa_faseAz.real+dxH, sec_directa_faseAz.imag+dyH, sec_inversa_faseAz.real, sec_inversa_faseAz.imag, angles="xy", scale_units="xy", scale=1, color="khaki")
     secI_faseBz = ax.quiver(sec_directa_faseBz.real+dxH, sec_directa_faseBz.imag+dyH, sec_inversa_faseBz.real, sec_inversa_faseBz.imag, angles="xy", scale_units="xy", scale=1, color="khaki")
-    secI_faseCz = ax.quiver(sec_directa_faseCz.real+dxH, sec_directa_faseCz.imag+dyH, sec_inversa_faseCz.real, sec_inversa_faseCz.imag, angles="xy", scale_units="xy", scale=1, color="khaki")
-    
+    secI_faseCz = ax.quiver(sec_directa_faseCz.real+dxH, sec_directa_faseCz.imag+dyH, sec_inversa_faseCz.real, sec_inversa_faseCz.imag, angles="xy", scale_units="xy", scale=1, color="khaki") 
     fig.tight_layout()
     st.pyplot(fig, clear_figure=True)
+    
+with col3:
+    modH, angH = cart2polar(dxH, dyH)
+    st.markdown("Secuencia homopolar: ")
+    st.latex(f"U_{{a0}}=U_{{b0}}=U_{{c0}}={modH:.2f}∠{angH:.2f}")
+    
+    modD_fa, angD_fa = cart2polar(sec_directa_faseAz.real, sec_directa_faseAz.imag)
+    modD_fb, angD_fb = cart2polar(sec_directa_faseBz.real, sec_directa_faseBz.imag)
+    modD_fc, angD_fc = cart2polar(sec_directa_faseCz.real, sec_directa_faseCz.imag)
+    st.markdown("Secuencia directa: ")
+    st.latex(f"U_{{a1}}={modD_fa:.2f}∠{angD_fa:.2f}")
+    st.latex(f"U_{{b1}}={modD_fb:.2f}∠{angD_fb:.2f}")
+    st.latex(f"U_{{c1}}={modD_fc:.2f}∠{angD_fc:.2f}")
+    
+    modI_fa, angI_fa = cart2polar(sec_inversa_faseAz.real, sec_inversa_faseAz.imag)
+    modI_fb, angI_fb = cart2polar(sec_inversa_faseBz.real, sec_inversa_faseBz.imag)
+    modI_fc, angI_fc = cart2polar(sec_inversa_faseCz.real, sec_inversa_faseCz.imag)
+    st.markdown("Secuencia inversa: ")
+    st.latex(f"U_{{a2}}={modI_fa:.2f}∠{angI_fa:.2f}")
+    st.latex(f"U_{{b2}}={modI_fb:.2f}∠{angI_fb:.2f}")
+    st.latex(f"U_{{c2}}={modI_fc:.2f}∠{angI_fc:.2f}")
     
     
     
